0a93504de7b435aacd4700dc5bb0552f435a275a
jaspersn <jasper.s.newkirk@gmail.comgit>
2023-01-08 20:49:40 -0800

    Altered documentation

--- charge.js
+++ charge.js
@@ -6,10 +6,10 @@ class Charge extends AnimatedEntity {
     /**
      * Creates a new instance of a charge item.
      * @param {GameEngine} game The {@link GameEngine} instance to be associated with this charge.
-     * @param {*} tag The type of charge to be placed. One of "Earth", "Fire", "Ice", "Electric", or "Disabled".
-     * @param {*} x The x-coordinate associated with the top-left corner of the charge's sprite in the current {@link GameEngine.ctx} context.
-     * @param {*} y The y-coordinate associated with the top-left corner of the charge's sprite in the current {@link GameEngine.ctx} context.
-     * @param {*} loop Whether the charge's animation loops over again, after having finished playing once.
+     * @param {String} tag The type of charge to be placed. One of "Earth", "Fire", "Ice", "Electric", or "Disabled".
+     * @param {number} x The x-coordinate associated with the top-left corner of the charge's sprite in the current {@link GameEngine.ctx} context.
+     * @param {number} y The y-coordinate associated with the top-left corner of the charge's sprite in the current {@link GameEngine.ctx} context.
+     * @param {boolean} loop Whether the charge's animation loops over again, after having finished playing once.
      */
     constructor(game, tag, x, y, loop = true) {
         super(game, "./assets/graphics/item/charge", tag, x, y, 8*PARAMS.SCALE, 8*PARAMS.SCALE, loop);
