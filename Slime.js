f4ced0e60745ffbbc37ece85212e3ddf2ee1daf7
Xavier Hines <xmhines01@gmail.com>
2023-01-13 13:24:36 -0800

    Comment

--- Slime.js
+++ Slime.js
@@ -4,7 +4,7 @@ class Slime{
         //ISSUE: this was built to animate with Marriots code. Change to work with ours.
         //this.animator = new animator(ASSET_MANAGER.getAsset("./assests/graphics/characters/Slime-sheet4.png"), 0, 0, 24, 16, 6, .3);
         
-        // add way to see slime that works with animate identity or import animator from Marriot
+        // add way to see slime that works with animate identity or import animator from Mar
 
         this.x = 448;
         this.y = 320;
