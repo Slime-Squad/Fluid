a33abdebcbd819c3a1960c01ce40426f12dc68ec
jaspersn <jasper.s.newkirk@gmail.comgit>
2023-01-22 14:48:22 -0800

    Added mapping tools for level creation

---
+++ mapping_tools/stone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="stone" tilewidth="8" tileheight="8" tilecount="78" columns="13">
+ <image source="../assets/graphics/map/stone.png" width="104" height="48"/>
+</tileset>
---
+++ mapping_tools/tools.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tools" tilewidth="8" tileheight="8" tilecount="324" columns="18">
+ <image source="tools.png" width="144" height="144"/>
+</tileset>
