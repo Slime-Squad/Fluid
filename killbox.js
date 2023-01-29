e4b360146377c4df5244e0ae4d5403e7aa5c85bb
jaspersn <jasper.s.newkirk@gmail.comgit>
2023-01-28 16:26:57 -0800

    Added KillBox

---
+++ killbox.js
@@ -0,0 +1,34 @@
+/**
+ * Class representation of a "bottomless pit" hitbox, which kills the player on contact.
+ * @author Jasper Newkirk
+ */
+class KillBox {
+    /**
+     * Constructs a new entity responsible for killing the player on contact.
+     * @param {number} x The x-coordinate associated with the top-left corner of the killbox's hitbox on the canvas.
+     * @param {number} y The y-coordinate associated with the top-left corner of the killbox's hitbox on the canvas.
+     */
+    constructor(x, y) {
+        Object.assign(this, { x, y });
+        this.width = 8*PARAMS.SCALE;
+        this.height = 8*PARAMS.SCALE;
+        this.hitbox = new HitBox(this.x, this.y, this.width, this.height);
+    }
+    
+    /**
+     * Function called on every clock tick.
+     */
+    update() {
+            
+    }
+
+    /**
+     * Draws the debug information for the current killbox. Called on every clock tick.
+     * @param {CanvasRenderingContext2D} ctx The canvas to which the information will be displayed upon.
+     */
+    draw(ctx) {
+        if (PARAMS.DEBUG) {
+            ctx.fillText("KILL", this.x - GAME.camera.x, this.y - GAME.camera.y);
+        }
+    }
+}
