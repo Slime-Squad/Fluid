8278bb7239724a07945790b8899968368d46b20e
jaspersn <jasper.s.newkirk@gmail.comgit>
2023-01-28 16:24:29 -0800

    Added functionality for camera animations

--- scenemanager.js
+++ scenemanager.js
@@ -9,7 +9,9 @@ class SceneManager {
     constructor() {
         this.x = 0;
         this.y = 0;
+        this.deathScreen = new DeathScreen();
         this.start = Date.now();
+        this.isFrozen = false;
         this.frames = 0;
         this.loadTest();
     }
@@ -19,14 +21,16 @@ class SceneManager {
      */
     loadTest() {
         const world = ASSET_MANAGER.getAsset("./assets/world/world/level.world");
+        GAME.addEntity(this.deathScreen);
 
-        Object.keys(world.rooms).forEach((roomName) => {
-            const room = world.rooms[roomName];
-            const order = ["fg", "entity", "map", "bg"];
-            order.forEach((layer) => {
+        const order = ["fg", "entity", "map", "bg"];
+        order.forEach((layer) => {
+            Object.keys(world.rooms).forEach((roomName) => {
+                const room = world.rooms[roomName];
+                console.log("loading", layer)
                 if (room.tiles[layer]) { // layer exists
                     room.tiles[layer].forEach((tile) => {
-                        PARAMS.GAME.addEntity(tile);
+                        GAME.addEntity(tile);
                     });
                 }
             });
@@ -37,8 +41,15 @@ class SceneManager {
      * Function called on every clock tick.
      */
     update() {
-        this.x = PARAMS.GAME.slime.x - PARAMS.WIDTH/2;
-        this.y = PARAMS.GAME.slime.y - PARAMS.HEIGHT/2;
+        if (this.isFrozen) {
+            this.elapsedFreezeTime += GAME.clockTick;
+            if (this.elapsedFreezeTime >= this.freezeTimer) this.unfreeze();
+        } else {
+            this.x = Math.round(GAME.slime.x - PARAMS.WIDTH/2);
+            this.y = Math.round(GAME.slime.y - PARAMS.HEIGHT/2);
+        }
+        this.deathScreen.x = this.x - 3*PARAMS.SCALE;
+        this.deathScreen.y = this.y - 3*PARAMS.SCALE;
         // Update fps
         this.frames++;
         if (Math.floor((Date.now()-this.start)/1000) > 0) {
@@ -46,6 +57,7 @@ class SceneManager {
             this.fps = this.frames;
             this.frames = 0;
         }
+        
     }
 
     /**
@@ -54,11 +66,34 @@ class SceneManager {
      */
     draw(ctx) {
         // DEBUG INFO
+        if (this.isFrozen) this.freezeAnimation(ctx, this);
         if (PARAMS.DEBUG) {
             ctx.font = "30px segoe ui";
             ctx.fillStyle = "white";
             ctx.fillText("CAM: x=" + this.x + " y=" + this.y, 10, 30);
             ctx.fillText("FPS:" + this.fps, PARAMS.WIDTH-120, 30);
         }
+    }
+
+    /**
+     * Freezes the camera and allows for movement independent of the player.
+     * @param {number} time The amount of time to freeze the camera (in seconds).
+     * @param {function} animation A function passed the canvas and current camera instance. Will be called on this camera's {@link SceneManager.update} function during the freeze.
+     * @param {function} callback A function to be executed upon the end of the animation, when the camera is no longer frozen.
+     */
+    freeze(time, animation, callback) {
+        this.isFrozen = true;
+        this.freezeTimer = time;
+        this.elapsedFreezeTime = 0;
+        this.freezeCallback = callback;
+        this.freezeAnimation = animation;
+    }
+
+    /**
+     * Places the camera's focus back to the player, and calls the callback function from {@link SceneManager.freeze}.
+     */
+    unfreeze() {
+        this.isFrozen = false;
+        this.freezeCallback();
     }
 }
