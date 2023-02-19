b172f4d08a3aed33eae03c21287bafb0b1659c11
jaspersn <jasper.s.newkirk@gmail.comgit>
2023-02-19 13:43:00 -0800

    Updated posters for clearity

---
+++ mapping_tools/tilesets/stone_bg_decal_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="stone_bg_decal_1" tilewidth="8" tileheight="8" tilecount="78" columns="13">
+ <image source="../../assets/graphics/map/stone_bg_decal_1.png" width="104" height="48"/>
+</tileset>
