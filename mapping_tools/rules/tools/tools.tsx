ca5338aea0c1f67d9a9b20257e1d6b4b743fd5f4
jaspersn <jasper.s.newkirk@gmail.comgit>
2023-02-19 10:49:36 -0800

    Refactored mapping tools organization for better scalability

---
+++ mapping_tools/rules/tools/tools.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tools" tilewidth="8" tileheight="8" tilecount="324" columns="18">
+ <image source="tools.png" width="144" height="144"/>
+ <tile id="0">
+  <properties>
+   <property name="Name" value="spawn"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Name" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Name" value="Electric Charge"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Name" value="Fire Charge"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Name" value="Earth Charge"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Name" value="Ice Charge"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Name" value="Batterflea"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Name" value="Bubble"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Name" value="Magmasquito"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Name" value="Skiwi"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Name" value="KillBox"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Name" value="Checkpoint"/>
+  </properties>
+ </tile>
+</tileset>
--- mapping_tools/tilesets/ice.tsx
+++ mapping_tools/tilesets/ice.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="ice" tilewidth="8" tileheight="8" tilecount="78" columns="13">
- <image source="../assets/graphics/map/ice.png" width="104" height="48"/>
+ <image source="../../assets/graphics/map/ice.png" width="104" height="48"/>
 </tileset>
