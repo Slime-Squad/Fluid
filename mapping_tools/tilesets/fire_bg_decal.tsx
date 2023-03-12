968c42220f32d2a4b5c2f66eb3d9a4cd19d4e187
Xavier Hines <xmhines01@gmail.com>
2023-03-12 12:31:07 -0700

    Background and foreground with changes to fire bubble level.

---
+++ mapping_tools/tilesets/fire_bg_decal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fire_bg_decal" tilewidth="8" tileheight="8" tilecount="78" columns="13">
+ <image source="../../assets/graphics/map/fire_bg_decal.png" width="104" height="48"/>
+</tileset>
